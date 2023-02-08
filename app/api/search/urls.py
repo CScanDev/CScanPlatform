516c1443b996563dfc5776a54b7551239c557e9a
Denis Popov <argsot3211@mail.ru>
2023-02-08 15:16:25 +0400

    test route fix

--- app/api/search/urls.py
+++ app/api/search/urls.py
@@ -1,8 +1,8 @@
 from rest_framework.routers import DefaultRouter
 
-from api.search.views import SearchView, TestAllCourseView
+from api.search.views import SearchView, TestView
 
 router = DefaultRouter()
 router.register("", SearchView, basename="main")
-router.register("test", TestAllCourseView, basename="test")
+router.register("test", TestView, basename="test")
 urlpatterns = router.urls
